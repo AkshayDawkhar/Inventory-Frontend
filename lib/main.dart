4fa682e120ba1437a0c3b13f17fdecc60be8775a
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-03-05 16:27:08 +0530

    create resizable widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'my app',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter '),
     );
   }
 }
@@ -61,6 +61,23 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<int> items = <int>[
+    900,
+    900,
+    900,
+    900,
+    900,
+    900,
+    800,
+    700,
+    600,
+    500,
+    400,
+    300,
+    200,
+    100
+  ];
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -69,6 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    final w = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -76,39 +94,44 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+          child: GridView.builder(
+              gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: w < 550 ?1:2,
+                childAspectRatio: 3/2 ,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 10,
+
+              ),
+              addRepaintBoundaries: true,
+              itemCount: items.length,
+              padding: EdgeInsets.all(12),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height,
+                  color:  w < 600 ?Colors.blue[items[index]] : Colors.red[items[index]] ,
+                  child: Text('$w'),
+                );
+              })
+
+          // child: ListView.separated(
+          //     itemBuilder: (BuildContext context, int index) {
+          //       return Container(
+          //         // padding: EdgeInsets.all(12),
+          //         height: 50,
+          //         color: Colors.pink[items[index]],
+          //       );
+          //     },
+          //     separatorBuilder: (BuildContext context, int index) =>
+          //         const Divider(),
+          //     itemCount: items.length,
+          //     padding: EdgeInsets.all(12),
+          // )
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.remove),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
