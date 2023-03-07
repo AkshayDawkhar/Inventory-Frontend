5c0afe92abcf47657124b2d4365da0f77c56bc49
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-03-07 23:37:20 +0530

    added MyApp in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'homepage.dart';
-
+import 'routes.dart';
 void main() {
-  runApp(const MyAppDemo());
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(routerConfig: goRouter);
+  }
 }
