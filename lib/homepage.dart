7489968a0ff2aeb8e2a89e2e1f22fc28c1f79cd4
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-03-07 23:38:51 +0530

    added easy_sidemenu

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
 import 'routes.dart';
 import 'bottom_nav_bar.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 
-class MyAppDemo extends StatefulWidget {
-  const MyAppDemo({Key? key}) : super(key: key);
 
-  @override
-  State<MyAppDemo> createState() => _MyAppDemoState();
-}
-
-class _MyAppDemoState extends State<MyAppDemo> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: goRouter);
-  }
-}
+List<int> items = <int>[
+  900,
+  900,
+  900,
+  900,
+  900,
+  900,
+  800,
+  700,
+  600,
+  500,
+  400,
+  300,
+  200,
+  100,
+  900,
+  900,
+  900,
+  900,
+  900,
+  900,
+  800,
+  700,
+  600,
+  500,
+  400,
+  300,
+  200,
+  100,
+  900,
+  900,
+  900,
+  900,
+  900,
+  900,
+  800,
+  700,
+  600,
+  500,
+  400,
+  300,
+  200,
+  100,
+  900,
+  900,
+  900,
+  900,
+  900,
+  900,
+  800,
+  700,
+  600,
+  500,
+  400,
+  300,
+  200,
+  100,
+];
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -34,86 +81,125 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  List<int> items = <int>[
-    900,
-    900,
-    900,
-    900,
-    900,
-    900,
-    800,
-    700,
-    600,
-    500,
-    400,
-    300,
-    200,
-    100,900,
-    900,
-    900,
-    900,
-    900,
-    900,
-    800,
-    700,
-    600,
-    500,
-    400,
-    300,
-    200,
-    100,900,
-    900,
-    900,
-    900,
-    900,
-    900,
-    800,
-    700,
-    600,
-    500,
-    400,
-    300,
-    200,
-    100,900,
-    900,
-    900,
-    900,
-    900,
-    900,
-    800,
-    700,
-    600,
-    500,
-    400,
-    300,
-    200,
-    100,
-  ];
 
   @override
   Widget build(BuildContext context) {
-    final w = MediaQuery.of(context).size.width;
-    int a =1;
-
-    return Scaffold(
-      bottomNavigationBar: BottomNavbBar(context,a),
+    final w = MediaQuery
+        .of(context)
+        .size
+        .width;
+    int a = 1;
+    SideMenuController page= SideMenuController();
+    List<SideMenuItem> sideitems = [
+      SideMenuItem(
+        // Priority of item to show on SideMenu, lower value is displayed at the top
+        priority: 0,
+        title: 'Dashboard',
+        // onTap: (){},
+        icon: Icon(Icons.home),
+        badgeContent: Text(
+          '3',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      SideMenuItem(
+        priority: 1,
+        title: 'Settings',
+        // onTap: (){},
+        icon: Icon(Icons.settings),
+      ),
+      SideMenuItem(
+        priority: 2,
+        title: 'Exit',
+        // onTap: (){},
+        icon: Icon(Icons.exit_to_app),
+      ),
+    ];
+    return w < 1000 ? Scaffold(
+      bottomNavigationBar: BottomNavbBar(context, a),
       // endDrawer: Drawer(child: Icon(Icons.send_time_extension),),
       drawer: w < 600
-          ?  Drawer(
-              backgroundColor: Color.fromARGB(12, 204, 15, 15),
-              child:Text('hii'),
-            )
+          ? Drawer(
+        child: Text('hii'),
+      )
           : const Drawer(
-              child: Text('no hi'),
-            ),
+        child: Text('no hi'),
+      ),
       appBar: AppBar(
-        actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.person))],
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.person))],
         title: Text(widget.title),
+      ),
+      body: Row(
+        children: [
+          SideMenu(
+            // Page controller to manage a PageView
+            controller: page,
+            // Will shows on top of all items, it can be a logo or a Title text
+            // title: Image.asset('assets/images/easy_sidemenu.png'),
+            // Will show on bottom of SideMenu when displayMode was SideMenuDisplayMode.open
+            footer: Text('demo'),
+            // Notify when display mode changed
+            onDisplayModeChanged: (mode) {
+              print(mode);
+            },
+            style: SideMenuStyle(
+              openSideMenuWidth: 200
+            ),
+            // List of SideMenuItem to show them on SideMenu
+            items: sideitems,
+          ),
+          Expanded(
+            child: Center(
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: w < 750 ? 1 : 2,
+                      childAspectRatio: 4 / 2,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 10,
+                    ),
+                    addRepaintBoundaries: true,
+                    addAutomaticKeepAlives: true,
+                    itemCount: items.length,
+                    padding: EdgeInsets.all(12),
+                    itemBuilder: (BuildContext context, int index) {
+                      print(index);
+                      return Container(
+                        alignment: Alignment.center,
+                        height: MediaQuery
+                            .of(context)
+                            .size
+                            .height,
+                        color: w < 600
+                            ? Colors.blue[items[index]]
+                            : Colors.red[items[index]],
+                        child: Text('${DateTime.now()}'),
+                      );
+                    })),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.handyman),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ):body(context, widget);
+  }
+}
+
+Widget body(BuildContext context, final widget) =>
+    Scaffold(
+      bottomNavigationBar: BottomNavbBar(context, 1),
+      // endDrawer: Drawer(child: Icon(Icons.send_time_extension),),
+      drawer: const Drawer(child: Text('no hi')),
+      appBar: AppBar(
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.person))],
+        title: Text(widget.title.toString()),
       ),
       body: Center(
           child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: w < 750 ? 1 : 2,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
                 childAspectRatio: 4 / 2,
                 crossAxisSpacing: 20,
                 mainAxisSpacing: 10,
@@ -123,21 +209,20 @@ class _MyHomePageState extends State<MyHomePage> {
               itemCount: items.length,
               padding: EdgeInsets.all(12),
               itemBuilder: (BuildContext context, int index) {
-              print(index);
+                print(index);
                 return Container(
                   alignment: Alignment.center,
-                  height: MediaQuery.of(context).size.height,
-                  color: w < 600
-                      ? Colors.blue[items[index]]
-                      : Colors.red[items[index]],
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height,
+                  color: Colors.red[items[index]],
                   child: Text('${DateTime.now()}'),
                 );
               })),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.handyman),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-}
