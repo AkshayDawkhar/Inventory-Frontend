f8166a83afd5d767c7a6de1d69bf0e7db9bc3311
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-03-09 10:22:22 +0530

    create Scaffold for mobile and desktop

---
+++ lib/responsive/desktop_scaffold.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DesktopScaffold extends StatefulWidget {
+  const DesktopScaffold({Key? key}) : super(key: key);
+
+  @override
+  State<DesktopScaffold> createState() => _DesktopScaffoldState();
+}
+
+class _DesktopScaffoldState extends State<DesktopScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.red,
+    );
+  }
+}
---
+++ lib/responsive/mobile_scaffold.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MobileScaffold extends StatefulWidget {
+  const MobileScaffold({Key? key}) : super(key: key);
+
+  @override
+  State<MobileScaffold> createState() => _MobileScaffoldState();
+}
+
+class _MobileScaffoldState extends State<MobileScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.green,
+    );
+  }
+}
