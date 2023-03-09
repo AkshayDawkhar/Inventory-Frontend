fc94c46f5f5e5c3a4f41c126adf743db019e16a8
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-03-09 11:27:38 +0530

    change in routes as per layouts

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'homepage.dart';
+import 'responsive/responsive_layout.dart';
+import 'responsive/desktop_scaffold.dart';
+import 'responsive/mobile_scaffold.dart';
 
 GoRouter goRouter = GoRouter(routes: [
   GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
+        return ResponsiveLayout(
+            desktopScaffold: const DesktopScaffold(),
+            mobileScaffold: const MobileScaffold());
+      }),
+  GoRoute(
+      path: '/me',
+      builder: (BuildContext context, GoRouterState state) {
         return const MyHomePage(title: 'ME');
       },
-      routes:[
+      routes: [
         GoRoute(
           path: 'you/:me',
           builder: (BuildContext context, GoRouterState state) {
             String me = state.params['me']!;
-            return  MyHomePage(title: me);
+            return MyHomePage(title: me);
           },
-        ),]
-  ),
-]
-);
+        ),
+      ]),
+]);
