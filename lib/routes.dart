aa0667c8b1720346981db104d21355ff0b70258c
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-03-10 13:24:15 +0530

    removed old home page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'homepage.dart';
 import 'responsive/responsive_layout.dart';
 import 'responsive/desktop_scaffold.dart';
 import 'responsive/mobile_scaffold.dart';
@@ -13,18 +12,4 @@ GoRouter goRouter = GoRouter(routes: [
             desktopScaffold: const DesktopScaffold(),
             mobileScaffold: const MobileScaffold());
       }),
-  GoRoute(
-      path: '/me',
-      builder: (BuildContext context, GoRouterState state) {
-        return const MyHomePage(title: 'ME');
-      },
-      routes: [
-        GoRoute(
-          path: 'you/:me',
-          builder: (BuildContext context, GoRouterState state) {
-            String me = state.params['me']!;
-            return MyHomePage(title: me);
-          },
-        ),
-      ]),
 ]);
