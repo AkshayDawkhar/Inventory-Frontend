03e9c3d090a2220f9869d781fc318a36521ecd66
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-04-08 21:12:16 +0530

    changes in products page

--- lib/responsive/desktop_account_page.dart
+++ lib/responsive/desktop_account_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inventory/constants.dart';
+String mode = 'worker';
 
 class DesktopAccountPage extends StatefulWidget {
   const DesktopAccountPage({Key? key}) : super(key: key);
@@ -9,14 +10,37 @@ class DesktopAccountPage extends StatefulWidget {
 }
 
 class _DesktopAccountPageState extends State<DesktopAccountPage> {
+  List<DropdownMenuItem> acc = const [
+    DropdownMenuItem(
+      value: 'worker',
+      child: Text('worker'),
+    ),
+    DropdownMenuItem(value: 'admin', child: Text('admin')),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: myAppBar('ACCOUNT'),
+      appBar: myAppBar(mode.toUpperCase()),
       body: Row(
         children: [
           Expanded(flex: 1, child: myDrawer(context, 3)),
-          Expanded(flex: 6, child: Text('account'))
+          Expanded(
+              flex: 6,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  DropdownButton(
+                    items: acc,
+                    onChanged: (item) {
+                      mode = item;
+                      setState(() {
+                      });
+                    },
+                    value: mode,
+                  ),
+                ],
+              ))
         ],
       ),
     );
--- lib/responsive/desktop_products_page.dart
+++ lib/responsive/desktop_products_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:inventory/constants.dart';
 
+import '../helper.dart';
+
 class DesktopProductsPage extends StatefulWidget {
   const DesktopProductsPage({Key? key}) : super(key: key);
 
@@ -9,14 +11,63 @@ class DesktopProductsPage extends StatefulWidget {
 }
 
 class _DesktopProductsPageState extends State<DesktopProductsPage> {
+  late Future<List> items;
+  @override
+  void initState() {
+    // TODO: implement initState
+    // super.initState();
+    // items= ;
+    Future.delayed(Duration(seconds: 10));
+    items = HttpHelper().fetchItems();
+    print('object');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar('PRODUCTS'),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () async{
+            List a = await HttpHelper().fetchItems();
+            // sleep(const Duration(seconds: 3));
+          },
+          backgroundColor: Colors.blueGrey,
+          icon: Icon(
+            Icons.add,
+            size: 30,
+          ),
+          label: Text('NEW'),
+        ),
       body: Row(
         children: [
           Expanded(flex: 1, child: myDrawer(context, 4)),
-          Expanded(flex: 6, child: Text('Products'))
+          Expanded(flex: 6, child: FutureBuilder(
+              future: items,
+              builder: (context,snapshot)  {
+                if(snapshot.hasData){
+                  return Center(
+                      child: GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            childAspectRatio: 4 / 2,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 10,
+                          ),
+                          addRepaintBoundaries: true,
+                          addAutomaticKeepAlives: true,
+                          itemCount: snapshot.data!.length,
+                          padding: const EdgeInsets.all(12),
+                          itemBuilder: (BuildContext context, int index) {
+                            // return cen(snapshot.data!.elementAt(index)['pid'].toString());
+                            return myContainer(context, false, index,snapshot.data!.elementAt(index)['pid'].toString(),snapshot.data!.elementAt(index)['dname'].toString());
+                          })
+                  )
+                  ;
+                }
+                else{
+                  return const CircularProgressIndicator();
+
+                }
+              }))
         ],
       ),
     );
